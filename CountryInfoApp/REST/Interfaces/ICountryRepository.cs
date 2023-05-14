9d29a14e039b2ddd1f893a20970b89121f964483
Jakub Podlesny <s20540@pjwstk.edu.pl>
2023-05-14 18:28:12 +0200

    Created Client Rest API to get data about countries

---
+++ CountryInfoApp/REST/Interfaces/ICountryRepository.cs
@@ -0,0 +1,9 @@
+﻿using CountryInfoApp.Shared.JsonWrappers;
+
+namespace CountryInfoApp.REST.Interfaces
+{
+    public interface ICountryRepository
+    {
+        Task<List<CountryDetails>> GetCountryDetailsAsync(string countryName);
+    }
+}
---
+++ CountryInfoApp/REST/Repositories/CountryRepository.cs
@@ -0,0 +1,23 @@
+﻿using CountryInfoApp.HttpHelpers;
+using CountryInfoApp.REST.Interfaces;
+using CountryInfoApp.Shared.JsonWrappers;
+
+namespace CountryInfoApp.REST.Repositories
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly string _baseURL = "https://restcountries.com/v3.1/name/";
+        private readonly IHttpService _httpService;
+
+        public CountryRepository(IHttpService httpService)
+        {
+            _httpService = httpService;
+        }
+
+        public async Task<List<CountryDetails>> GetCountryDetailsAsync(string countryName)
+        {
+            var httpResponse = await _httpService.GetAsync<List<CountryDetails>>($"{_baseURL}{countryName}");
+            return httpResponse.Response;
+        }
+    }
+}
